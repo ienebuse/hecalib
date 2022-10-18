2c8df272005f3658f61c3b433fdbe6867efbafe2
Ikenna Enebuse <enebusei@coventry.ac.uk>
2022-10-19 00:13:53 +0100

    added method Park

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 from sim_data import *
-from hecalib import Cal_Tsai,  Cal_Daniilidis, Cal_Chou, Cal_Li
+from hecalib import Cal_Tsai,  Cal_Daniilidis, Cal_Chou, Cal_Park, Cal_Li
 
 A = [HA1,HA2,HA3,HA4]
 B = [HB1,HB2,HB3,HB4]
@@ -8,7 +8,8 @@ X = Hx
 # cal = Cal_Tsai()
 # cal = Cal_Chou()
 # cal = Cal_Daniilidis()
-cal = Cal_Li()
+# cal = Cal_Li()
+cal = Cal_Park()
 R,T, rel_err, abs_err = cal.calibrate(A,B,X)
 
 print(R)
--- hecalib/__init__.py
+++ hecalib/__init__.py
@@ -2,3 +2,4 @@ from .tsai import Cal_Tsai
 from .daniilidis import Cal_Daniilidis
 from .chou import Cal_Chou
 from .li import Cal_Li
+from .park import Cal_Park
